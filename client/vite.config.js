52456b65a3e9ef0218c477ddaaaefed215ea4de3
AbdulAhadSiddiqui-0786 <abdulahadsiddiqui23@gmail.com>
2025-07-16 01:08:00 +0530

    now deploying

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,9 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// client/vite.config.js
+import { defineConfig, loadEnv } from "vite";
+import react from "@vitejs/plugin-react";
+import { fileURLToPath, URL } from "node:url";
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
-export default defineConfig({
-  
-  plugins: [react(),tailwindcss(),],
-})
+export default defineConfig(({ mode }) => {
+  const env   = loadEnv(mode, process.cwd());
+  const isDev = mode === "development";
+
+  return {
+    plugins: [react(),tailwindcss()],
+    server: {
+      proxy: {
+        "/api": {
+          target: isDev ? "http://localhost:10000" : env.VITE_API_URL,
+          changeOrigin: true,
+        },
+      },
+    },
+  };
+});
